7986d30029f3f0fda7928af1cab6737f75704adc
Your Name <you@example.com>
2024-09-11 21:13:15 -0300

    projeto inicial

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<?php
+echo '<header>
+    <div class="cabecalho">
+        <div class="text-center">
+            <div class="text-center"></div>';
+            include 'includes/menu.php';
+        echo '</div>
+    </header>';
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include 'includes/dados.php';?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="assets/css/styles.css">
+    </head>
+    <body>
+    <?php include 'includes/header.php';?>
+    <div class="position-relative">
+    <img src="assets/images/petexpress.jpeg" class="dogdirige"  alt="">
+    </div>
+        <div class="container">
+            <main>
+                <div>
+                <img src="assets/images/aifolou.jpg" class="aifolou"  alt="">
+                <p class="caixaprot">Temos caixas protetoras de todos os tipos e tamanhos!</p>
+                <img src="assets/images/amigo.jpg" class="amigo"  alt="">
+                <p class="caixaamigo">Transportamos diversos animais!</p>
+                <img src="assets/images/entrega.jpg" class="entrega"  alt="">
+                </div>
+            </main>
+            <?php include 'includes/footer.php';?>
+        </div>
+    </body>
+</html>
